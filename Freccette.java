d6d69a66623b83dd9d700a8cfde7462f680d29c0
NorvegianGoat <riccardomioli@gmail.com>
2018-01-08 20:28:56 +0100

    Finished all the game modes

--- Freccette.java
+++ Freccette.java
@@ -1,3 +1,9 @@
+//Cose da fare:
+//Cambiare l'ordine dei metodi... Conviene prima dire il numero dei punti e poi per cosa moltiplicarli in modo da passare in ingresso al metodo il valore
+//Fare un unico metodo per la gestione del punteggio e diversi per la gestione dellúltimo titolo
+//Controllaree costanti suggerite da S.
+//Pulizia del codice generale
+
 import java.util.*;
 
 public class Freccette {
@@ -25,7 +31,6 @@ public class Freccette {
 	}
     }
 
-    //In futuro cambiare l'ordine dei metodi... Conviene prima dire il numero dei punti e poi per cosa moltiplicarli in modo da passare in ingresso al metodo il valore
     private static int Moltiplicatori() {
        System.out.println("Il giocatore ha ottenuto un double o un triple?");
             int moltiplicatore = input.nextInt();
@@ -53,7 +58,7 @@ public class Freccette {
     private static void StraightOut() { 
         System.out.println("Nella Modalita' straight out i giocatori si sfidano a chi per primo arriva a zero punti.");
         int puntiIniziali0= 501, puntiIniziali1= 501;
-        while (puntiIniziali0>0&puntiIniziali1>0) {
+        while (puntiIniziali0>0 & puntiIniziali1>0) {
             System.out.println("Inserire il punteggio di Giocatore 0");
             for (int tiri= 3; tiri>0; tiri--) {
             int puntiGiocatore= Moltiplicatori();
@@ -70,10 +75,10 @@ public class Freccette {
             for (int tiri= 3; tiri>0; tiri--) {
             int puntiGiocatore= Moltiplicatori();
             puntiIniziali1=puntiIniziali1-puntiGiocatore;
-            if (puntiIniziali0<0) {
-                puntiIniziali0=puntiIniziali0+puntiGiocatore;
+            if (puntiIniziali1<0) {
+                puntiIniziali1=puntiIniziali1+puntiGiocatore;
                 System.out.println("Hai spalato");
-            } else if (puntiIniziali0==0) {
+            } else if (puntiIniziali1==0) {
                 break;
             }
             System.out.println("Attualmente il Giocatore 1 ha "+ puntiIniziali1);  
@@ -87,25 +92,103 @@ public class Freccette {
         while (puntiIniziali0>0 & puntiIniziali1>0) {
             System.out.println("Inserire il punteggio di Giocatore 0");
             for (int tiri= 3; tiri>0; tiri--) {
-            Moltiplicatori();
-            int puntiTurno = input.nextInt();
-            System.out.println("Attualmente il Giocatore 1 ha "+ (puntiIniziali0=puntiIniziali0-puntiTurno));
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali0=puntiIniziali0-puntiGiocatore;
+            if (puntiIniziali0<0) {
+                puntiIniziali0=puntiIniziali0+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali0==0) {
+                if (puntiGiocatore%2==0) {
+                break;
+            }
+            }
+            System.out.println("Attualmente il Giocatore 0 ha "+ puntiIniziali0);
             }
             System.out.println("Inserire il punteggio di Giocatore 1");
             for (int tiri= 3; tiri>0; tiri--) {
-            Moltiplicatori();
-            int puntiTurno = input.nextInt();
-            System.out.println("Attualmente il Giocatore 1 ha "+ (puntiIniziali1=puntiIniziali1-puntiTurno));
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali1=puntiIniziali1-puntiGiocatore;
+            if (puntiIniziali1<0) {
+                puntiIniziali1=puntiIniziali1+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali1==0) {
+                if (puntiGiocatore%2==0) {
+                break;
+            }
+            }
+            System.out.println("Attualmente il Giocatore 1 ha "+ puntiIniziali1);  
             }
         }
     }
 
     private static void TripleOut() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Nella modalita' double out i giocatori si sfidano a chi per primo arriva a zero punti colpendo,con l'ultimo tiro, l'anello del double out");
+       int puntiIniziali0= 501, puntiIniziali1= 501;
+        while (puntiIniziali0>0 & puntiIniziali1>0) {
+            System.out.println("Inserire il punteggio di Giocatore 0");
+            for (int tiri= 3; tiri>0; tiri--) {
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali0=puntiIniziali0-puntiGiocatore;
+            if (puntiIniziali0<0) {
+                puntiIniziali0=puntiIniziali0+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali0==0) {
+                if (puntiGiocatore%3==0) {
+                break;
+                }
+            }
+            System.out.println("Attualmente il Giocatore 0 ha "+ puntiIniziali0);
+            }
+            System.out.println("Inserire il punteggio di Giocatore 1");
+            for (int tiri= 3; tiri>0; tiri--) {
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali1=puntiIniziali1-puntiGiocatore;
+            if (puntiIniziali1<0) {
+                puntiIniziali1=puntiIniziali1+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali1==0) {
+                if (puntiGiocatore%3==0) {
+                break;
+                }
+            }
+            System.out.println("Attualmente il Giocatore 1 ha "+ puntiIniziali1);  
+            }
+        }
     }
 
     private static void MasterOut() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Nella modalita' double out i giocatori si sfidano a chi per primo arriva a zero punti colpendo,con l'ultimo tiro, l'anello del double out");
+       int puntiIniziali0= 501, puntiIniziali1= 501;
+        while (puntiIniziali0>0 & puntiIniziali1>0) {
+            System.out.println("Inserire il punteggio di Giocatore 0");
+            for (int tiri= 3; tiri>0; tiri--) {
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali0=puntiIniziali0-puntiGiocatore;
+            if (puntiIniziali0<0) {
+                puntiIniziali0=puntiIniziali0+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali0==0) {
+                if (puntiGiocatore==50) {
+                break;
+                }
+            }
+            System.out.println("Attualmente il Giocatore 0 ha "+ puntiIniziali0);
+            }
+            System.out.println("Inserire il punteggio di Giocatore 1");
+            for (int tiri= 3; tiri>0; tiri--) {
+            int puntiGiocatore= Moltiplicatori();
+            puntiIniziali1=puntiIniziali1-puntiGiocatore;
+            if (puntiIniziali1<0) {
+                puntiIniziali1=puntiIniziali1+puntiGiocatore;
+                System.out.println("Hai spalato");
+            } else if (puntiIniziali1==0) {
+                if (puntiGiocatore==50) {
+                break;
+                }
+            }
+            System.out.println("Attualmente il Giocatore 1 ha "+ puntiIniziali1);  
+            }
+        }
     }
-
+    
 }
